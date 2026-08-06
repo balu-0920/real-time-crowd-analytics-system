81f9fa3393306a35277c899f26e732a48d0cb12a
balu_20 <balunijampatnam09@gmail.com>
2026-08-06 21:26:28 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -36,7 +36,7 @@ export default function Login({ onLogin }) {
 
     try {
 
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
