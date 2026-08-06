aee3f3deaad3d6280a6470f17904824d6156766d
balu_20 <balunijampatnam09@gmail.com>
2026-08-06 22:51:07 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -60,11 +60,17 @@ app.post("/api/auth/login", async (req, res) => {
     if (!match)
       return res.status(401).json({ error: "Invalid password" });
 
-    jwt.sign(
-    { id: user._id, role: user.role },
-    process.env.JWT_SECRET,
-    { expiresIn: "1d" }
+    const token = jwt.sign(
+  { id: user._id, role: user.role },
+  process.env.JWT_SECRET,
+  { expiresIn: "1d" }
 );
+
+res.json({
+  token,
+  username: user.username,
+  role: user.role,
+});
 
     res.json({
       username: user.username,
